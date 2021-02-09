741181e8628037c0f5572511c8f74713b2fa8e9e
katherinex <kathexio@gmail.com>
2021-02-08 22:39:46 -0500

    Wrap the elements os the component in a Fragment to avoid adding another element when we do not want this

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,10 +1,13 @@
+
+import { Fragment } from 'react';
+
 // Functional Component FC
 const PrimeraApp = () => {
-    return ( // Siempre deben estar contenidos en un solo elemento para hacer el return, puede ser un div o un Fragment
-        <div>
+    return ( // Siempre deben estar contenidos en un solo elemento para hacer el return, puede ser un div o un Fragment, el fragment no pone ningín elemento
+        <Fragment >
             <h1>Hola Mundo</h1>
             <p>Mi primera aplicación</p>
-        </div>  
+        </Fragment>
     );
         
 };
