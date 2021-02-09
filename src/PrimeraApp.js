ea4867894a8d37bf550e9b2fa7287c044d15012a
katherinex <kathexio@gmail.com>
2021-02-08 22:41:48 -0500

    Wrap the elements os the component in void tags

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,13 +1,17 @@
 
-import { Fragment } from 'react';
+// import { Fragment } from 'react';
 
 // Functional Component FC
 const PrimeraApp = () => {
-    return ( // Siempre deben estar contenidos en un solo elemento para hacer el return, puede ser un div o un Fragment, el fragment no pone ningín elemento
-        <Fragment >
+    
+    // Siempre deben estar contenidos en un solo elemento para hacer el return, 
+    // puede ser un div o un Fragment, el fragment no pone ningún elemento
+    // o, si no queremos importar el Fragment, simplemente dejamos etiquetas vacías <> </>, react ya entiende que esto es un fragment
+    return ( 
+        <> 
             <h1>Hola Mundo</h1>
             <p>Mi primera aplicación</p>
-        </Fragment>
+        </>
     );
         
 };
