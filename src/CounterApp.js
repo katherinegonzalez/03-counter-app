9659c322a9e5433d0a98f9b5241e34c9b44da53d
katherinex <kathexio@gmail.com>
2021-02-10 22:32:21 -0500

    useSate hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,16 +1,24 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+    // useState retorna un arreglo con 2 valores
+    const [ counter, setCounter ] = useState(0); // revisar esto en el archivo de desestructuración
+    
+
     // handleAdd
     const handleAdd = (event) => {
         console.log(event);
+        setCounter( counter + 1 ); // No funciona con counter ++ porque no debemos muar el objeto
+        // ó también se puede hacerlo así:
+        // setCounter( (c)=> c + 1);
     }
 
     return(
         <>
         <h1>CounterApp</h1>
-        <h2> { value } </h2>
+        <h2> { counter } </h2>
         {/* <button onClick={ (event) => { handleAdd(event) }}> +1 </button> es lo mismo qué: por que cuando se pasa el mismo paràmetro se puede dejar solo el nombre de la función*/}
         <button onClick={ handleAdd }> +1 </button> 
         </>
