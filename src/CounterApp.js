415d253720504036872fd5baf0bc6525a8eb52ce
katherinex <kathexio@gmail.com>
2021-02-10 22:17:34 -0500

    Imlement event in button

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,17 @@ import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
 
+    // handleAdd
+    const handleAdd = (event) => {
+        console.log(event);
+    }
+
     return(
         <>
         <h1>CounterApp</h1>
         <h2> { value } </h2>
+        {/* <button onClick={ (event) => { handleAdd(event) }}> +1 </button> es lo mismo qué: por que cuando se pasa el mismo paràmetro se puede dejar solo el nombre de la función*/}
+        <button onClick={ handleAdd }> +1 </button> 
         </>
     );
 
