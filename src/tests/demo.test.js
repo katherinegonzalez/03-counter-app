6d3bba93e74dd175ac093ece2f94a39bb5624071
katherinex <kathexio@gmail.com>
2021-02-15 21:10:11 -0500

    Unit tests

---
+++ src/tests/demo.test.js
@@ -0,0 +1,15 @@
+describe('Pruebas en el archivo demo.test.js', () => {
+    test('the strings should be equal', () => {
+    
+        // 1. Inicialización
+        const mensaje = 'Hola Mundo';
+    
+        // 2. Estímulo
+        const mensaje2 = `Hola Mundo`;
+    
+        //3. Observar el comportamiento
+        expect(mensaje).toBe(mensaje2);
+    });
+});
+
+
